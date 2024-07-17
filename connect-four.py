8461cacad2c275473a8bb28f48fd5bc7efa1ee6a
Jack C. Lloyd <jc.lloyd@hotmail.co.uk>
2024-07-17 21:14:05 +0100

    Python (Refactor)
    Refactored the Python 3 implementation to adhere to the AQA coding style.

--- connect-four.py
+++ connect-four.py
@@ -2,44 +2,29 @@
 
 import random
 
-DISC_PLAYER_ONE = '\u25CB'
-DISC_PLAYER_TWO = '\u25CF'
-SLOT_EMPTY = '\u25CC'
+DISC_ONE = '\u25CB' # Red
+DISC_TWO = '\u25CF' # Yellow
+EMPTY    = '\u25CC'
 
 COLUMNS, ROWS = 7, 6
-Board = [[SLOT_EMPTY for _ in range(ROWS)] for _ in range(COLUMNS)]
+Board = [[EMPTY for _ in range(ROWS)] for _ in range(COLUMNS)]
 
 PlayerOneName = ""
 PlayerTwoName = ""
 PlayerOneScore = 0
 PlayerTwoScore = 0
-PlayerOneTurn = True
-HasWonOrDrawn = False
-Answer = ''
-
-def PlayerDisc(IsPlayerOne):
-	if IsPlayerOne:
-		return DISC_PLAYER_ONE
-	else:
-		return DISC_PLAYER_TWO
-
-def PlayerName(IsPlayerOne):
-	if IsPlayerOne:
-		return PlayerOneName
-	else:
-		return PlayerTwoName
 
 def PrintScore():
-	print(PlayerOneName + ": " + str(PlayerOneScore))
-	print(PlayerTwoName + ": " + str(PlayerTwoScore))
+	print(PlayerOneName, PlayerOneScore)
+	print(PlayerTwoName, PlayerTwoScore)
 	print()
 
-def ClearBoard(Board):
+def ClearBoard():
 	for Column in range(COLUMNS):
 		for Row in range(ROWS):
-			Board[Column][Row] = SLOT_EMPTY
+			Board[Column][Row] = EMPTY
 
-def PrintBoard(Board):
+def PrintBoard():
 	print() # Header
 	print("  1 2 3 4 5 6 7  ")
 	print("|---------------|")
@@ -55,20 +40,20 @@ def ValidSlot(Slot):
 	Slots = [str(Count) for Count in range(1, COLUMNS + 1)]
 	return Slot in Slots
 
-def TrySlot(Board, Slot, Disc):
+def TrySlot(Slot, Disc):
 	if not ValidSlot(Slot):
 		print("Slot " + Slot + " is invalid!")
 		return False
 	Column = int(Slot) - 1 # Index
 	for Row in range(ROWS):
-		if Board[Column][Row] is SLOT_EMPTY:
+		if Board[Column][Row] is EMPTY:
 			Board[Column][Row] = Disc
 			return True
 	else:
 		print("Slot " + Slot + " is full!")
 		return False
 
-def CheckHorizontal(Board, Disc):
+def CheckHorizontal(Disc):
 	for Column in range(COLUMNS - 3):
 		for Row in range(ROWS):
 			for Count in range(Column, Column + 4):
@@ -79,7 +64,7 @@ def CheckHorizontal(Board, Disc):
 	else:
 		return False
 
-def CheckVertical(Board, Disc):
+def CheckVertical(Disc):
 	for Column in range(COLUMNS):
 		for Row in range(ROWS - 3):
 			for Count in range(Row, Row + 4):
@@ -90,7 +75,7 @@ def CheckVertical(Board, Disc):
 	else:
 		return False
 
-def CheckDiagonal(Board, Disc):
+def CheckDiagonal(Disc):
 	for Column in range(COLUMNS - 3):
 		for Row in range(ROWS - 3):
 			for Count in range(4): # Forwards
@@ -106,20 +91,20 @@ def CheckDiagonal(Board, Disc):
 	else:
 		return False
 
-def CheckWin(Board, Disc):
-	if CheckHorizontal(Board, Disc):
+def CheckWin(Disc):
+	if CheckHorizontal(Disc):
 		return True
-	elif CheckVertical(Board, Disc):
+	elif CheckVertical(Disc):
 		return True
-	elif CheckDiagonal(Board, Disc):
+	elif CheckDiagonal(Disc):
 		return True
 	else:
 		return False
 
-def CheckDraw(Board):
+def CheckDraw():
 	for Column in range(COLUMNS):
 		for Row in range(ROWS):
-			if Board[Column][Row] == SLOT_EMPTY:
+			if Board[Column][Row] == EMPTY:
 				return False
 	else:
 		return True
@@ -131,26 +116,40 @@ if __name__ == "__main__":
 	PlayerOneName = input("Enter the name of player one: ")
 	PlayerTwoName = input("Enter the name of player two: ")
 	PlayerOneTurn = FlipCoin()
+	Answer = ''
 	while Answer not in ['N', 'n']:
-		print(PlayerName(PlayerOneTurn) + " starts!")
-		ClearBoard(Board)
-		PrintBoard(Board)
+		if PlayerOneTurn:
+			print(PlayerOneName, "starts!")
+		else:
+			print(PlayerTwoName, "starts!")
+		ClearBoard()
+		PrintBoard()
 		HasWonOrDrawn = False
 		while not HasWonOrDrawn:
-			Slot = input(PlayerName(PlayerOneTurn) + ", enter a slot: ")
-			while not TrySlot(Board, Slot, PlayerDisc(PlayerOneTurn)):
-				Slot = input("Enter another slot: ")
-			PrintBoard(Board)
-			if CheckWin(Board, PlayerDisc(PlayerOneTurn)):
+			if PlayerOneTurn:
+				Name = PlayerOneName
+				Disc = DISC_ONE
+			else:
+				Name = PlayerTwoName
+				Disc = DISC_TWO
+			Slot = input(Name + ", enter a slot: ")
+			while not TrySlot(Slot, Disc):
+				Slot = input(Name + ", enter another slot: ")
+			PrintBoard()
+			if CheckWin(Disc):
+				HasWonOrDrawn = True
 				if PlayerOneTurn:
 					PlayerOneScore += 1
 				else:
 					PlayerTwoScore += 1
-				print(PlayerName(PlayerOneTurn) + " wins!\n")
+				print(Name, "wins!")
+				print()
+			elif CheckDraw():
 				HasWonOrDrawn = True
-			elif CheckDraw(Board):
-				print("Draw!\n")
-				HasWonOrDrawn = True
-			PlayerOneTurn = not PlayerOneTurn
+				print("Draw!")
+				print()
+			else:
+				PlayerOneTurn = not PlayerOneTurn
 		PrintScore()
-		Answer = input("Play again?\nY/N: ")
+		print("Play again? (Y/N)")
+		Answer = input("Answer: ")
